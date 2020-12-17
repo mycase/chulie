57a2d5d8437dd95dc4841868cb49a8eb1a037d57
Ke Zhang <ke.zhang@mycase.com>
2020-12-17 12:34:01 -0800

    Make type exports explicit

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export * from './lib/config';
+export type { Config } from './lib/config';
+export type { Message } from './lib/message';
 export * from './lib/json_object';
-export * from './lib/message';
 export * from './lib/sqs_processor';
